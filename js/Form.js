e86c924e6fff967fe21753685f99fab843ffa0fd
Sotiris Ioannou <me@sotiris.engineer>
2017-03-06 21:05:49 +0100

    Finished login form

--- js/Form.js
+++ js/Form.js
@@ -1,15 +1,27 @@
 import React from 'react';
 
 class Form extends React.Component {
-    formSubmited(event) {
+    constructor(props) {
+        super(props);
+        this.formSubmited = this.formSubmited.bind(this);
+    }
+    formSubmited() {
         event.preventDefault();
-        console.log(this);
+        /* console.log(this);*/
+
+        const loginInfo = {
+            username: this.username.getDOMNode().value,
+            password: this.password.getDOMNode().value
+        }
+
+        console.log(loginInfo);
+        this.sForm.getDOMNode().reset(0); //reset form
     }
     render() {
         return(
-            <form onSubmit={(e) => this.formSubmited(e)}>
-                <input type='text' placeholder='Username'/>
-                <input type='password' placeholder='Password'/>
+            <form ref={(input) => {this.sForm = input;}} onSubmit={this.formSubmited}>
+                <input ref={(input) => {this.username = input;}} type='text' placeholder='Username'/>
+                <input ref={(input) => {this.password = input;}} type='password' placeholder='Password'/>
                 <button type='submit'>Submit</button>
             </form>
         )
