d70f7c036b2b35de37a9ed7d8fa1025d1afb89ab
Sotiris Ioannou <me@sotiris.engineer>
2017-03-06 18:57:22 +0100

    init

---
+++ js/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hello from './hello.js';
+
+let App = React.createClass({
+  render(){
+    return (
+      <Hello />
+    );
+  }
+});
+
+React.render(
+  <App />,
+  document.body
+  )
