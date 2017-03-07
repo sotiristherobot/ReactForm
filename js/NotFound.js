327e7ae79dbb6f1fedd1ececc1efaf504581e9b9
Sotiris Ioannou <me@sotiris.engineer>
2017-03-07 19:46:02 +0100

    Added react router and not found component.

---
+++ js/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+    render() {
+        return(
+           <h3> Not Found </h3> 
+        )
+    }
+}
+export default NotFound;
--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Form from './Form';
+import NotFound from './NotFound.js';
+import {render} from 'react-dom'; 
 
-let App = React.createClass({
-  render(){
+import {BrowserRouter, Match, Miss} from 'react-router';
+
+const Root = () => {
     return (
-        <Form />
-    );
-  }
-});
-
-React.render(<App />,document.body)
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern='/' component={Form}/>
+                <Miss component={NotFound}/>
+            </div>
+        </BrowserRouter>
+    )
+}
+render(<Root/>, document.querySelector('#react'));
