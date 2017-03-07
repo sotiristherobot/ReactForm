e47d6198eab9fd23632330ea4a4023424d0b58dd
Sotiris Ioannou <me@sotiris.engineer>
2017-03-07 12:21:53 +0100

    Make the initial POST request to get the jwd token

--- js/Form.js
+++ js/Form.js
@@ -7,16 +7,42 @@ class Form extends React.Component {
     }
     formSubmited() {
         event.preventDefault();
-        /* console.log(this);*/
 
+        //get values from form
         const loginInfo = {
             username: this.username.getDOMNode().value,
             password: this.password.getDOMNode().value
         }
 
+        const params = 'email=' + loginInfo.username + '&password=' + loginInfo.password;
+
+        const makeRequest = function(method, url, params) {
+            return new Promise((resolve, reject) => {
+                const request = new XMLHttpRequest();
+                request.open(method, url, true);
+                request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+                request.onload = () => {
+                    if (request.status == 200) {
+                        resolve(request.response);
+                    }
+                    else {
+                        reject(Error(request.statusText));
+                    }
+                }
+                request.send(params);
+            });
+        }
+        /* let params = 'email=challenge@i2x.ai&password=pass123';*/
+
+        makeRequest('POST', 'https://i2x-challenge.herokuapp.com/core/login/ ', params )
+            .then((val)=> {
+                console.log(val);
+            });
+
         console.log(loginInfo);
         this.sForm.getDOMNode().reset(0); //reset form
-    }
+        }
     render() {
         return(
             <form ref={(input) => {this.sForm = input;}} onSubmit={this.formSubmited}>
